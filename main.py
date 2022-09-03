11fd730a7549e30ab03d106a329c70ac66b4aa6a
SohamNagi <nagisoham@gmail.com>
2022-09-03 19:01:55 -0400

    Sentiment Analysis

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from pygooglenews import GoogleNews
 
+#Classes
 gn = GoogleNews(lang = 'en', country = 'US')
+analyzer = SentimentIntensityAnalyzer()
 
 prompt=input('What is The Topic You Want Information About: ')
 
 articles=gn.search(prompt)
 
 for item in articles['entries']:
-    print(item['title'])
+    vs = analyzer.polarity_scores(item)
+    print("{:-<65} {}".format(item, str(vs)))
